bb23a69fb55f1c46f10f4c0b76835f5cf5df09e7
stevenAnto <ecalcinap@unsa.edu.pe>
2023-05-04 17:30:34 -0500

    implementacion de una clase que solo define-Interface-

--- poo/ClaseX.cpp
+++ poo/ClaseX.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-struct Date {
-  int y, m, d; // year, month, day
-  Date(int y, int m, int d){ // check for valid date and initialize
-    this->y=y;
-    this->m=m;
-    this->d=d;
-  }
-  void add_day(int n); // increase the Date by n days
+class Date{
+  public:
+    Date(int y,int m, int d);
+    void add_day(int n);
+    int month();
+  private:
+      int y,m,d;
 };
-
+Date::Date(int yy,int mm,int dd)
+  :y{yy},m{mm},d{dd}{}
+void Date::add_day(int n){
+  d++;
+}
+int Date:: month(){
+  return m;
+}
 int main(){
   Date today {12,24,2007}; // oops! run-time error
   Date last {2000,12,31}; // OK (colloquial style)
